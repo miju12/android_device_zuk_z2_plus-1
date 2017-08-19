62f6b7a88277d8772f8a64bcf2135b0d21e70c2c
Luca Stefani <luca020400@lineageos.org>
2017-08-19 13:17:46 +0200

    z2_plus: Update common_full_phone path
    Change-Id: I6d60ee5643181781ba8b386f1f16cfb59610f35c

--- lineage.mk
+++ lineage.mk
@@ -21,8 +21,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from z2_plus device
 $(call inherit-product, device/zuk/z2_plus/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_z2_plus
 PRODUCT_DEVICE := z2_plus
