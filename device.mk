8f41bae5bc2089ac615bf0d804e85d47bd6f5451
joshuous <joshuous@gmail.com>
2017-09-09 05:32:03 +0000

    z2_plus: Bringup VNDK
    Signed-off-by: joshuous <joshuous@gmail.com>

--- device.mk
+++ device.mk
@@ -287,6 +287,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl
 
+# VNDK-SP:
+PRODUCT_PACKAGES += \
+    vndk-sp
+
 # WiFi HAL
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service
